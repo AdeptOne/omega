7481da5a24cd030c960892176aa75a29b12e9b91
Arkadij Gorenkov <53056534+AdeptOne@users.noreply.github.com>
2026-03-05 20:47:29 +0500

    Добавлен проект для QuantiX(сервис учета).

---
+++ Omega.QuantiX/Omega.QuantiX.Infrastructure.Postgres/Models/Common/ItemModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Omega.QuantiX.Infrastructure.Postgres.Models.Common;
+
+public abstract class ItemModel
+{
+    [Key]
+    public Guid ItemId { get; set; } = Guid.CreateVersion7();
+}
---
+++ Omega.QuantiX/Omega.QuantiX.Infrastructure.Postgres/Models/ProductModel.cs
@@ -0,0 +1,8 @@
+﻿using Omega.QuantiX.Infrastructure.Postgres.Models.Common;
+
+namespace Omega.QuantiX.Infrastructure.Postgres.Models;
+
+public class ProductModel : ItemModel
+{
+    
+}
